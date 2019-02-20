f6cb3cede01883fc6eb770a265d166e2dc1ba986
Louise Edvardsson <edvardsson.louise@gmail.com>
2019-02-20 21:56:06 +0100

    changes to homepage

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("p").click(function(){
+        $(this).hide();
+    });
+});
+
+$(document).ready(function(){
+    $(".footer").click(function(){
+        $(this).hide();
+    });
+});
